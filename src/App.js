3f2f6f8b1ad210b2c84a0add1cd8b3e4f8f2fb6b
Josh Kincheloe <jkincheloe@gsandf.com>
2020-03-29 02:02:21 -0500

    Removed Build folder

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { GlobalStyles } from './global';
 import { Landing } from './pages';
 
 const App = () => (
-  <Router basename="/portfolio">
+  <Router basename="/">
     <>
       <GlobalStyles />
       <Switch>
