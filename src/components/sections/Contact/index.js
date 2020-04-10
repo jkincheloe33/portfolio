1cd91ef03404f97a4e945f5a963e89094d545181
Josh Kincheloe <jkincheloe@gsandf.com>
2020-04-10 01:23:51 -0500

    Created new partial Contact section

--- src/components/sections/Contact/index.js
+++ src/components/sections/Contact/index.js
@@ -1,54 +1,101 @@
-import React, { Suspense, useCallback, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
-import { Canvas as CanvasBase } from 'react-three-fiber';
-import { theme } from '../../../global';
-import Model from './Model';
+import { H1, media, theme } from '../../../global';
 
-const { color } = theme;
+const { color, easing } = theme;
 
-const Canvas = styled(CanvasBase)`
-  min-height: 100vh;
+// prettier-ignore
+const Title = styled(H1)`
+  color: ${p => p.animate ? color.yellow : color.white};
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  position: relative;
+  text-align: center;
+  transition: color 10ms;
+  transition-delay: 2000ms;
+
+  span {
+    overflow: hidden;
+    position: relative;
+    transform: rotateZ(5deg) translateY(165%);
+    transition: transform 1000ms ${easing.easeIn};
+
+    &::before,
+    &::after {
+      background-color: ${color.yellow};
+      content: '';
+      height: 100%;
+      left: 0%;
+      position: absolute;
+      top: 0%;
+      transform: ${p => (p.animate ? 'translateY(-200%)' : 'translateY(100%)')};
+      transition: transform 1500ms cubic-bezier(0.95, 0.02, 0.52, 0.82);
+      transition-delay: 1000ms;
+      width: 100%;
+    }
+
+    &::after {
+      background-color: ${color.black};
+      transform: ${p => (p.animate ? 'translateY(-100%)' : 'translateY(200%)')};
+    }
+  }
+
+  ${p =>
+    p.animate &&
+    `
+    span {
+      transform: translateY(0) rotateZ(0);
+    }
+  `}
+
+  ${media.up.lg`
+    font-size: 200px;
+    line-height: 225px;
+  `}
 `;
 
 const Wrapper = styled.div`
-  min-height: 100vh;
+  padding-bottom: 200px;
   position: relative;
   width: 100%;
-
-  &::before,
-  &::after {
-    background: linear-gradient(${color.black}, transparent);
-    content: '';
-    height: 150px;
-    left: 0;
-    position: absolute;
-    right: 0;
-    top: 0;
-    z-index: 1;
-  }
-
-  &::after {
-    background-image: linear-gradient(transparent, ${color.black});
-    bottom: 0;
-    top: auto;
-  }
 `;
 
 const Contact = () => {
-  const mouse = useRef([0, 0]);
-  const onMouseMove = useCallback(
-    ({ clientX: x, clientY: y }) =>
-      (mouse.current = [x - window.innerWidth / 2, y - window.innerHeight / 2]),
-    []
-  );
+  const [animate, setAnimate] = useState(false);
+  const compRef = useRef(null);
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    handleScroll();
+    window.addEventListener('resize', handleScroll());
+    return () => {
+      window.removeEventListener('resize', handleScroll());
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleScroll = () => {
+    if (compRef.current) {
+      document.addEventListener('scroll', () => {
+        const ref = compRef.current;
+        // const height = ref.getBoundingClientRect().height;
+        const scroll = ref.getBoundingClientRect().top;
+
+        if (!animate && scroll < 800) {
+          setAnimate(true);
+        }
+      });
+    }
+  };
+  console.log(animate);
 
   return (
-    <Wrapper>
-      <Canvas camera={{ position: [0, 0, 0] }} onMouseMove={onMouseMove}>
-        <Suspense fallback={null}>
-          <Model mouse={mouse} position={[0, -2, 0]} rotation={[0, -0.6, 0]} />
-        </Suspense>
-      </Canvas>
+    <Wrapper ref={compRef}>
+      <Title animate={animate} ref={titleRef}>
+        <span>Let's Party</span>
+      </Title>
     </Wrapper>
   );
 };
