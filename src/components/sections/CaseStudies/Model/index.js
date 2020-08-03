2a3856b2277c211778a568117054085e0ccada18
Josh Kincheloe <jkincheloe33@gmail.com>
2020-08-03 17:02:42 -0500

    Base textures and materials for cube

--- src/components/sections/CaseStudies/Model/index.js
+++ src/components/sections/CaseStudies/Model/index.js
@@ -1,11 +1,23 @@
 import React, { useRef } from 'react';
 import { useFrame } from 'react-three-fiber';
-import { theme } from '../../../../global';
+import * as THREE from 'three';
+// import { theme } from '../../../../global';
 
-const { color } = theme;
+// const { color } = theme;
+
+const images = [
+  './img/me3.jpg',
+  './img/me3.jpg',
+  './img/me3.jpg',
+  './img/me3.jpg',
+  './img/me3.jpg',
+  './img/me3.jpg'
+];
 
 function Model({ ...props }) {
+  const loader = new THREE.TextureLoader();
   const meshRef = useRef(null);
+  const textures = images.map(image => loader.load(image));
 
   useFrame(() => {
     if (meshRef.current) {
@@ -17,7 +29,9 @@ function Model({ ...props }) {
   return (
     <mesh {...props} ref={meshRef}>
       <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
-      <meshStandardMaterial attach="material" color={color.orange} />
+      {textures.map((texture, i) => (
+        <meshStandardMaterial attach="material" key={i} map={texture} />
+      ))}
     </mesh>
   );
 }
